e0ab9ee1baf716598a169cb51473ada7d9aea39d
WojtekSza <86256349+WojtekSza@users.noreply.github.com>
2023-10-07 19:04:16 +0200

    Add files via upload

---
+++ flights.py
@@ -0,0 +1,27 @@
+import logging
+import sys
+import tempfile
+import time
+from pprint import pprint
+
+import pendulum
+
+from airflow import DAG
+from airflow.decorators import task
+
+
+with DAG(
+    dag_id="Flights Database",
+    schedule=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["example"],
+) as dag:
+
+    # [START howto_operator_python]
+    @task(task_id="print_the_context")
+    def print_context():
+        print("test")
+        return "Whatever you return gets printed in the logs"
+
+    run_this = print_context()
